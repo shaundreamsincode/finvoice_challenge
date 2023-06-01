2d00cbdaa0236beafcf641717f68d8cc9af933c6
Shaun Carland <shaundreamsincode@gmail.com>
2023-05-31 22:28:34 -0400

    add invoice model w/out attachment (todo - add that)

---
+++ db/migrate/20230601022150_create_invoices.rb
@@ -0,0 +1,12 @@
+class CreateInvoices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :invoices do |t|
+      t.text :token, :string
+      t.decimal :amount
+      t.text :due_at
+      t.text :status
+      t.index :token, unique: true
+      t.timestamps
+    end
+  end
+end
