38d1753a266d660c3ecc25f9a1fecf6f0b90a6b2
Shaun Carland <shaundreamsincode@gmail.com>
2023-06-01 16:22:37 -0400

    add status to enum and remove has_secure_token

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,20 +1,27 @@
 class Invoice < ApplicationRecord
   class InvalidStatusError < StandardError; end
-  has_secure_token :token # todo - remove this
+
+  STATUSES = {
+    created: 'created',
+    rejected: 'rejected',
+    approved: 'approved',
+    purchased: 'purchased',
+    closed: 'closed'
+  }
 
   def purchase!
-    unless status == 'approved'
+    unless status == STATUSES[:approved]
       raise InvalidStatusError, 'Only approved invoices can be set to purchased'
     end
 
-    update!(status: 'purchased')
+    update!(status: STATUSES[:purchased])
   end
 
   def close!
-    unless status == 'purchased'
+    unless status == STATUSES[:purchased]
       raise InvalidStatusError, 'Only purchased invoices can be set to closed'
     end
 
-    update!(status: 'closed')
+    update!(status: STATUSES[:closed])
   end
 end
