c267bde8246b5a54bdb4a89030dfcdc70adbc9bd
Shaun Carland <shaundreamsincode@gmail.com>
2023-06-04 16:13:26 -0400

    format code with prettier

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,86 +1,88 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import InvoiceModal from "./InvoiceModal";
-import Container from 'react-bootstrap/Container'
-import Card from 'react-bootstrap/Card'
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [invoices, setInvoices] = useState([]);
-  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const [invoiceBeingEdited, setInvoiceBeingEdited] = useState(null);
 
   useEffect(() => {
-      axios.get('/api/v1/invoices').then((response) => {
-          setInvoices(response.data)
-      })
-  }, [])
+    axios.get("/api/v1/invoices").then((response) => {
+      setInvoices(response.data);
+    });
+  }, []);
 
-    // todo - fix formatting...
-    function openModal(invoice) {
-        setModalIsOpen(true)
-        setInvoiceBeingEdited(invoice)
+  function openModal(invoice) {
+    setModalIsOpen(true);
+    setInvoiceBeingEdited(invoice);
+  }
+
+  function closeModal(updatedInvoice = null) {
+    setModalIsOpen(false);
+    setInvoiceBeingEdited(null);
+
+    if (updatedInvoice) {
+      updateEditedInvoice(updatedInvoice);
     }
+  }
 
-    function closeModal(updatedInvoice=null) {
-      setModalIsOpen(false)
-        setInvoiceBeingEdited(null)
-
-        if (updatedInvoice) {
-            updateEditedInvoice(updatedInvoice)
-        }
-    }
-
-    function updateEditedInvoice(updatedInvoice) {
-      const invoiceIndexToUpdate = invoices.findIndex((invoice) => invoice.id === updatedInvoice.id)
-        invoices[invoiceIndexToUpdate] = updatedInvoice
-    }
+  function updateEditedInvoice(updatedInvoice) {
+    const invoiceIndexToUpdate = invoices.findIndex(
+      (invoice) => invoice.id === updatedInvoice.id
+    );
+    invoices[invoiceIndexToUpdate] = updatedInvoice;
+  }
 
   return (
     <Container className="m-2">
-        <Card>
-            <Card.Body>
-                test
+      <Card>
+        <Card.Body>
+          <table>
+            <thead>
+              <tr>
+                <th>Invoice #</th>
+                <th>Amount</th>
+                <th>Fees Accrued</th>
+                <th>Due Date</th>
+                <th>Status</th>
+                <th>View/Edit</th>
+              </tr>
+            </thead>
 
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Invoice #</th>
-                        <th>Amount</th>
-                        <th>Fees Accrued</th>
-                        <th>Due Date</th>
-                        <th>Status</th>
-                        <th>View/Edit</th>
-                    </tr>
-                    </thead>
-
-                    <tbody>
-                    {
-                        invoices.map((invoice) => {
-                            console.log(invoice)
-                            return(
-                                <tr key={invoice.token}>
-                                    <td>{ invoice.token }</td>
-                                    <td>{ invoice.amount }</td>
-                                    <td>{ invoice.fees_accrued }</td>
-                                    <td>{ invoice.due_at }</td>
-                                    <td>{ invoice.status }</td>
-                                    <th> <button onClick={() => openModal(invoice)}>
-                                        View/Edit
-                                    </button> </th>
-                                </tr>
-                            )
-                        })
-                    }
-                    </tbody>
-                </table>
-            </Card.Body>
-        </Card>
-        {
-            modalIsOpen && <InvoiceModal invoice={invoiceBeingEdited} modalIsOpen={modalIsOpen} onModalClose={closeModal}/>
-        }
-
+            <tbody>
+              {invoices.map((invoice) => {
+                return (
+                  <tr key={invoice.token}>
+                    <td>{invoice.token}</td>
+                    <td>{invoice.amount}</td>
+                    <td>{invoice.fees_accrued}</td>
+                    <td>{invoice.due_at}</td>
+                    <td>{invoice.status}</td>
+                    <th>
+                      {" "}
+                      <button onClick={() => openModal(invoice)}>
+                        View/Edit
+                      </button>{" "}
+                    </th>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </Card.Body>
+      </Card>
+      {modalIsOpen && (
+        <InvoiceModal
+          invoice={invoiceBeingEdited}
+          modalIsOpen={modalIsOpen}
+          onModalClose={closeModal}
+        />
+      )}
     </Container>
   );
 }
--- app/javascript/components/InvoiceModal.jsx
+++ app/javascript/components/InvoiceModal.jsx
@@ -1,72 +1,77 @@
 import React, { useState } from "react";
-import Modal from 'react-modal';
+import Modal from "react-modal";
 import axios from "axios";
-import Card from 'react-bootstrap/Card'
+import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 
 function InvoiceModal(props) {
-    const {invoice, modalIsOpen, onModalClose } = props;
-    const [invoiceBeingPurchased, setInvoiceBeingPurchased] = useState(false)
-    const [invoiceBeingClosed, setInvoiceBeingClosed] = useState(false)
+  const { invoice, modalIsOpen, onModalClose } = props;
+  const [invoiceBeingPurchased, setInvoiceBeingPurchased] = useState(false);
+  const [invoiceBeingClosed, setInvoiceBeingClosed] = useState(false);
 
-    const purchaseInvoice = () => {
-        axios.post(`/api/v1/invoices/${invoice.id}/purchase`).then((response) => {
-            onModalClose(response.data)
-        })
-    }
+  const purchaseInvoice = () => {
+    axios.post(`/api/v1/invoices/${invoice.id}/purchase`).then((response) => {
+      onModalClose(response.data);
+    });
+  };
 
-    const closeInvoice = () => {
-        axios.post(`/api/v1/invoices/${invoice.id}/close`).then((response) => {
-            onModalClose(response.data)
-        })
-    }
+  const closeInvoice = () => {
+    axios.post(`/api/v1/invoices/${invoice.id}/close`).then((response) => {
+      onModalClose(response.data);
+    });
+  };
 
+  return (
+    <>
+      <Modal isOpen={modalIsOpen} onRequestClose={onModalClose}>
+        <Container className="m-2">
+          <Card>
+            <Card.Body>
+              {!invoiceBeingPurchased && !invoiceBeingClosed && (
+                <div>
+                  invoice # {invoice.token} <br />
+                  Amount {invoice.amount} <br />
+                  Due Date {invoice.due_at} <br />
+                  Status {invoice.status} <br />
+                  Fees Accrued {invoice.fees_accrued} <br />
+                  Scan Name {invoice.scan_filename} <br />
+                  {invoice.status === "approved" && (
+                    <button onClick={() => setInvoiceBeingPurchased(true)}>
+                      Purchase Invoice
+                    </button>
+                  )}
+                  {invoice.status === "purchased" && (
+                    <button onClick={() => setInvoiceBeingClosed(true)}>
+                      Close Invoice
+                    </button>
+                  )}
+                </div>
+              )}
 
-    return(<>
-        <Modal isOpen={modalIsOpen} onRequestClose={onModalClose}>
-            <Container className='m-2'>
-                <Card>
-                    <Card.Body>
-                        {
-                            !invoiceBeingPurchased && !invoiceBeingClosed && <div>
-                                invoice # { invoice.token } <br/>
-                                Amount { invoice.amount } <br/>
-                                Due Date { invoice.due_at } <br/>
-                                Status { invoice.status } <br/>
-                                Fees Accrued { invoice.fees_accrued } <br/>
-                                Scan Name { invoice.scan_filename } <br/>
+              {invoiceBeingPurchased && (
+                <div>
+                  Are you sure you want to purchase the invoice {invoice.token}?
+                  <button onClick={purchaseInvoice}>Purchase</button>
+                </div>
+              )}
 
-                                {
-                                    invoice.status === 'approved' && <button onClick={() => setInvoiceBeingPurchased(true)}>Purchase Invoice</button>
-                                }
+              {invoiceBeingClosed && (
+                <div>
+                  Are you sure you want to close the invoice {invoice.token}?
+                  <button onClick={() => setInvoiceBeingClosed(false)}>
+                    Close
+                  </button>
+                  <button onClick={closeInvoice}>Close</button>
+                </div>
+              )}
 
-                                {
-                                    invoice.status === 'purchased' && <button onClick={() => setInvoiceBeingClosed(true)}>Close Invoice</button>
-                                }
-                            </div>
-                        }
-
-                        {
-                            invoiceBeingPurchased && <div>
-                                Are you sure you want to purchase the invoice { invoice.token }?
-                                <button onClick={purchaseInvoice}>Purchase</button>
-                            </div>
-                        }
-
-                        {
-                            invoiceBeingClosed && <div>
-                                Are you sure you want to close the invoice { invoice.token }?
-                                <button onClick={() => setInvoiceBeingClosed(false)}>Close</button>
-                                <button onClick={closeInvoice}>Close</button>
-                            </div>
-                        }
-
-                        <button onClick={onModalClose}>Cancel</button>
-                    </Card.Body>
-                </Card>
-            </Container>
-        </Modal>
-    </>)
+              <button onClick={onModalClose}>Cancel</button>
+            </Card.Body>
+          </Card>
+        </Container>
+      </Modal>
+    </>
+  );
 }
 
 export default InvoiceModal;
